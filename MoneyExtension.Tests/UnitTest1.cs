5a32c826ab3e15d9321c8e4c74f64bee76509af5
Isaque Albertini <albertiniisaque@gmail.com>
2025-05-06 15:04:14 -0300

    Finalizado a nova versão

---
+++ MoneyExtension.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoneyExtension;
+
+namespace MoneyExtension.Tests
+{
+    [TestClass]
+    public class MoneyExtensionTests
+    {
+        [TestMethod]
+        public void ShouldConvertDecimalToInt()
+        {
+            decimal valor = 279.98M;
+            var cents = valor.ToCents();
+
+            Assert.AreEqual(27998, cents);
+        }
+    }
+}
---
+++ MoneyExtension/MoneyExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace MoneyExtension
+{
+    public static class Money
+    {
+        public static int ToCents(this decimal amount)
+        {
+            if (amount < 0)
+                return 0;
+
+            var value = amount
+                .ToString("F2", CultureInfo.InvariantCulture)
+                .Replace(".", "");
+
+            if (string.IsNullOrEmpty(value))
+                return 0;
+
+            int.TryParse(value, out var result);
+            return result;
+        }
+    }
+}
